073a1ad73c2765ae8cb4a5a4bdee24644da1b3d5
Katherine Pitts <kvp06933@gmail.com>
2023-11-19 21:49:48 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AddressBookDN6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
